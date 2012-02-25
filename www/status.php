2113cb703490bce0000a58bd571dd958a63af267
Andrew M <fileformat@gmail.com>
2012-02-25 09:16:15 -0500

    status page work

--- www/status.php
+++ www/status.php
@@ -2,11 +2,16 @@
 
 	$retVal = array("success" => True);
 
+	$retVal["error_get_last()"] = error_get_last();
+	$retVal["error_reporting()"] = error_reporting();
+	$retVal["gc_enabled()"] = gc_enabled();
 	$retVal["get_loaded_extensions()"] = get_loaded_extensions();
-	$retVal["get_defined_constants()"] = get_defined_constants();
-	$retVal["ini_get_all()"] = ini_get_all();
+	$retVal["gmdate(DATE_ISO8601)"] = gmdate(DATE_ISO8601);
+	//too much: $retVal["get_defined_constants()"] = get_defined_constants();
+	//too much: $retVal["ini_get_all()"] = ini_get_all();
 	$retVal["memory_get_usage()"] = memory_get_usage();
 	$retVal["memory_get_peak_usage()"] = memory_get_peak_usage();
+	$retVal["microtime(True)"] = microtime(True);
 	$retVal["PHP_OS"] = PHP_OS;
 	$retVal["php_sapi_name()"] = php_sapi_name();
 	$retVal["php_uname()"] = php_uname();
