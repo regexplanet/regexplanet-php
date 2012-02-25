b05e23ab7841da081acb1f17606c556dc7044850
Andrew M <fileformat@gmail.com>
2012-02-25 09:02:09 -0500

    status page work

--- www/status.php
+++ www/status.php
@@ -2,8 +2,18 @@
 
 	$retVal = array("success" => True);
 
+	$retVal["get_loaded_extensions()"] = get_loaded_extensions();
+	$retVal["get_defined_constants()"] = get_defined_constants();
+	$retVal["ini_get_all()"] = ini_get_all();
+	$retVal["memory_get_usage()"] = memory_get_usage();
+	$retVal["memory_get_peak_usage()"] = memory_get_peak_usage();
+	$retVal["PHP_OS"] = PHP_OS;
+	$retVal["php_sapi_name()"] = php_sapi_name();
+	$retVal["php_uname()"] = php_uname();
 	$retVal["PHP_VERSION_ID"] = PHP_VERSION_ID;
 	$retVal["phpversion()"] = phpversion();
+	$retVal["sys_get_temp_dir()"] = sys_get_temp_dir();
 
+	header('Content-type: text/plain');
 	echo json_encode($retVal);
 ?>
