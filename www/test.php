db73ba89b87999fd110afb2017859bb506eeab48
Andrew M <fileformat@gmail.com>
2012-02-26 08:11:45 -0500

    Work on test.php

--- www/test.php
+++ www/test.php
@@ -4,10 +4,39 @@
 
 	$retVal["html"] = "<p>It isn't ready yet!</p>";
 
-	if (isset($_GET['callback']))
+	$regex = $_REQUEST["regex"];
+	$replacement = $_REQUEST["replacement"];
+
+	$html = "<table class=\"table table-bordered table-striped bordered-table zebra-striped\" style=\"width:auto;\">";
+	$html = $html . "\t<tbody>";
+
+	$html = $html . "\t\t<tr>\n";
+	$html = $html . "\t\t\t<td>Regular Expression</td>\n";
+	$html = $html . "\t\t\t<td>";
+	$html = $html . htmlspecialchars(regex);
+	$html = $html . "</td>";
+	$html = $html . "\t\t</tr>\n";
+
+	$html = $html . "\t\t<tr>\n";
+	$html = $html . "\t\t\t<td>as a PHP string</td>\n";
+	$html = $html . "\t\t\t<td>&quot;";
+	$html = $html . "&quot;</td>";
+	$html = $html . "\t\t</tr>\n";
+
+	$html = $html . "\t\t<tr>\n";
+	$html = $html . "\t\t\t<td>Replacement</td>\n";
+	$html = $html . "\t\t\t<td>";
+	$html = $html . htmlspecialchars(replacement);
+	$html = $html . "</td>\n";
+	$html = $html . "\t\t</tr>\n";
+
+	$html = $html . "\t</tbody>\n";
+	$html = $html . "</table>\n";
+
+	if (isset($_REQUEST['callback']))
 	{
 		header('content-type: application/javascript; charset=utf-8');
-		echo $_GET['callback'];
+		echo $_REQUEST['callback'];
 		echo "(";
 		echo json_encode($retVal);
 		echo ")";
