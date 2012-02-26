bbc455207cb689b4cc0542901f1be3cd793b0659
Andrew M <fileformat@gmail.com>
2012-02-25 22:52:29 -0500

    Placeholder test.php page

---
+++ www/test.php
@@ -0,0 +1,21 @@
+<?php
+
+	$retVal = array("success" => True);
+
+	$retVal["html"] = "<p>It isn't ready yet!</p>";
+
+	if (isset($_GET['callback']))
+	{
+		header('content-type: application/javascript; charset=utf-8');
+		echo $_GET['callback'];
+		echo "(";
+		echo $retVal;
+		echo ")";
+	}
+	else
+	{
+		header('content-type: application/json; charset=utf-8');
+		header("access-control-allow-origin: *");
+		echo $retVal;
+	}
+%>
