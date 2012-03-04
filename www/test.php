bf216bb9e77e220161868b6ea421a08a841c0780
Andrew M <fileformat@gmail.com>
2012-03-04 09:26:01 -0500

    fixed typo

--- www/test.php
+++ www/test.php
@@ -108,7 +108,7 @@
 			header("Access-Control-Allow-Origin: *");
 			header('Access-Control-Allow-Methods: POST, GET');
 			header('Access-Control-Max-Age: 604800');
-			echo json_encode($datal);
+			echo json_encode($data);
 		}
 	}
 
